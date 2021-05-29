f2f2e028cf0ef05f790d05092cbf2780d53bdd2a
yiping2333 <563262439@qq.com>
2021-05-29 15:40:34 +0800

    Revert "222"
    This reverts commit 610ca7f6

--- JavaWeb/src/main/java/com/syp/JavaWeb/TestServlet.java
+++ JavaWeb/src/main/java/com/syp/JavaWeb/TestServlet.java
@@ -8,7 +8,6 @@ public class TestServlet {
     public static void main(String[] args) {
         System.out.println("hello");
         System.out.println("hello2");
-        System.out.println("hello3");
 
     }
 }
