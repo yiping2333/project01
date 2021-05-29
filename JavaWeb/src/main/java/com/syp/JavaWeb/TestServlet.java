389435966f276d5d9deef351218faa115a51ce26
yiping2333 <563262439@qq.com>
2021-05-29 15:36:33 +0800

    222

--- JavaWeb/src/main/java/com/syp/JavaWeb/TestServlet.java
+++ JavaWeb/src/main/java/com/syp/JavaWeb/TestServlet.java
@@ -7,5 +7,7 @@ package com.syp.JavaWeb;
 public class TestServlet {
     public static void main(String[] args) {
         System.out.println("hello");
+        System.out.println("hello2");
+
     }
 }
