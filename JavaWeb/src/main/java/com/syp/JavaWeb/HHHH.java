d02afa40af2ea63e82058df40f1902922e2c4bd6
yiping2333 <563262439@qq.com>
2021-05-29 15:27:49 +0800

    111

---
+++ JavaWeb/src/main/java/com/syp/JavaWeb/HHHH.java
@@ -0,0 +1,8 @@
+package com.syp.JavaWeb;
+
+/**
+ * @author suyiping
+ * @create 2021-05-29 15:26
+ */
+public class HHHH {
+}
---
+++ JavaWeb/src/main/java/com/syp/JavaWeb/TestServlet.java
@@ -0,0 +1,11 @@
+package com.syp.JavaWeb;
+
+/**
+ * @author suyiping
+ * @create 2021-05-29 15:22
+ */
+public class TestServlet {
+    public static void main(String[] args) {
+        System.out.println("hello");
+    }
+}
